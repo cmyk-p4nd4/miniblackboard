3e0b6fc212042f35a974d9e1242a09c72361d9da
mrbakeryduck <70799443+mrbakeryduck@users.noreply.github.com>
2020-11-16 23:45:18 +0800

    don't know what happened

--- startup.js
+++ startup.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 const http = require('http');
-const PORT = 3000;
+const PORT = process.env.PORT || 5000
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/html_template.html'));
@@ -12,3 +12,9 @@ app.use('/', router);
 app.listen(3000, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('html_template'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
