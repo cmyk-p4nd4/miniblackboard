e0503ce3e8476b05eb24c6fe0f0c0ba11a7ee4a5
mrbakeryduck <70799443+mrbakeryduck@users.noreply.github.com>
2020-11-30 03:16:49 +0800

    Created Template Page

--- index.php
+++ index.php
@@ -1,11 +1,73 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Main Page</title>
-        <!-- <link rel="stylesheet" href="template.css"> -->
-    </head>
-    <body>
-        Main Page
-        <?php echo "Hello World" ?>
-    </body>
+<!doctype html>
+<html lang="en" class="h-100">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+<head>
+    <title>William Chan</title>
+    <link href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@1,585&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sticky-footer/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        @font-face {
+            font-family: 'Metropolis';
+            src: local('Metropolis Thin'), local('Metropolis-Thin'),
+                url('Metropolis-Thin.woff2') format('woff2'),
+                url('Metropolis-Thin.woff') format('woff'),
+                url('Metropolis-Thin.ttf') format('truetype');
+            font-weight: 100;
+            font-style: normal;
+        }
+    </style>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap" rel="stylesheet">
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer.css" rel="stylesheet">
+</head>
+
+<body class="d-flex flex-column h-100">
+    <!-- Begin page content -->
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <popup id="popup">
+                <center>
+                    <h2 style="font-family: 'Metropolis', sans-serif;">
+                        <font size="+3">
+                            UNDER CONSTRUCTION
+                        </font>
+                    </h2>
+                    <p style="font-family: 'Inter', sans-serif;">
+                        Please Come Back Soon!
+                        <br><br><br><br><br><br><br><br>
+                        <font style="font-family: 'Montserrat', sans-serif;" size="-2">2020 &copy; <b>Felix Tsoy &amp; Jimmy Cheung</b></font>
+                </center>
+                </p>
+                </center>
+            </popup>
+    </main>
+
+</body>
+
 </html>
