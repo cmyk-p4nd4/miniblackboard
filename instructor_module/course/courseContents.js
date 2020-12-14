e5b6e24c710ae4e0dc8531052aca92522a958eb5
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-14 11:23:25 +0800

    removed the functions for modify and delete, which are unnecessary

--- instructor_module/course/courseContents.js
+++ instructor_module/course/courseContents.js
@@ -17,28 +17,3 @@ function onViewChosen(contentid)
     document.body.appendChild(contentidForm);
     contentidForm.submit();
 }
-
-function onModifyChosen(contentid)
-{
-    //create an hidden form at courseContents.php and then pass the contentid to viewContent.php
-    var contentidForm = document.createElement("form");
-    contentidForm.setAttribute("id","contentidForm");
-    contentidForm.setAttribute("name","contentidForm");
-    contentidForm.setAttribute("method","post");
-    contentidForm.setAttribute("action","/eie4432/project/exam/modifyExam.php");
-
-    var contentidInput = document.createElement("input");
-    contentidInput.setAttribute("id","contentidInput");
-    contentidInput.setAttribute("name","modifyExamID");
-    contentidInput.setAttribute("value",contentid);
-    contentidInput.setAttribute("type","hidden");
-
-    contentidForm.appendChild(contentidInput);
-    document.body.appendChild(contentidForm);
-    contentidForm.submit();
-}
-
-function onDeleteChosen()
-{
-
-}
