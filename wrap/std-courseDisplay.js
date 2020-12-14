630cc406c59362d765099ef28fb3a960ea193098
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-15 03:48:36 +0800

    t

--- wrap/std-courseDisplay.js
+++ wrap/std-courseDisplay.js
@@ -1,8 +1,19 @@
-function onCourseClicked(startTime, duration, exam_name)
+function onCourseClicked(startDate, endDate, exam_name)
 {
     //three tasks
     //1. get necessary elements and contents
     //2. check if it is the time to do exam, if not prompt error
     //3. if yes, then navigate to the exam page.
-    alert("onCourseClicked!");
+    
+    
+    
+    
+    
+    
+    //var d = new Date();
+    //d.setTime(d.getTime() + 3600 * 1000);
+    //var dString = "expires="+d.toUTCString();
+    //document.cookie = "exam_name = " + exam_name + ";" + dString + ";path=/wrap";
+
+
 }
