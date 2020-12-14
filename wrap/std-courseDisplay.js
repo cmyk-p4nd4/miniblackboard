b606e8d47e0bfca69a444e2e596279a38700ca0a
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-15 05:07:40 +0800

    finished std-courseDisplay; newly add take_exam.php and css (15/12/2020 05:07)

--- wrap/std-courseDisplay.js
+++ wrap/std-courseDisplay.js
@@ -5,11 +5,69 @@ function onCourseClicked(starttime,duration,examid)
     //2. check if it is the time to do exam, if not prompt error
     //3. if yes, then navigate to the exam page.
     
-    alert(starttime);
-    alert(duration);
-    alert(examid);
+
+    //2. parse start time and end time
+    var startTime = new Date(starttime);
+    var startTimeUTC = startTime.toUTCString();
+
+    //extract the duration to seconds
+    var durationSec = 0;
+    var durationArray = duration.split(";");
+    var hours = durationArray[0];
+    var index = hours.indexOf('h');
+    hours = hours.substring(0,index);
+    hours = parseInt(hours);
+
+    durationSec = hours * 3600 * 1000;
+    
+    var minutes = durationArray[1];
+    index = minutes.indexOf('m');
+    minutes = minutes.substring(0,index);
+    minutes = parseInt(minutes);
+
+    durationSec += minutes * 60 * 1000;
+
+    var seconds = durationArray[2];
+    index = seconds.indexOf('s');
+    seconds = seconds.substring(0,index);
+    seconds = parseInt(seconds);
+
+    durationSec += seconds * 1000;
+
+    //set duration date object
+    var deadline = new Date();
+    deadline.setTime(startTime.getTime() + durationSec);
     
     
+
+    var canStart = true;
+    //check if the time is correct.
+    var currentTime = new Date();
+    if (currentTime < startTime)
+    {
+        canStart = false;
+        alert("The exam has not started!");
+    }
+
+    if (currentTime > deadline)
+    {
+        canStart = false;
+        alert("The exam has been ended!");
+    }
+
+    if (canStart)
+    {
+        //set cookie
+        var expireTime = new Date();
+        expireTime.setTime(expireTime.getTime()+36000*1000);
+        var expireString = "expires="+expireTime.toUTCString();
+        document.cookie = "exam_name = "+exam_name+";"+"start_time = "+startTimeUTC+";"
+        +"deadline = "+deadline+";"+expireString+";path=/wrap";
+
+        window.location.href = "https://web-miniblackboard.herokuapp.com/student_module/take_exam.php";
+        
+    }
+
     //var d = new Date();
     //d.setTime(d.getTime() + 3600 * 1000);
     //var dString = "expires="+d.toUTCString();
