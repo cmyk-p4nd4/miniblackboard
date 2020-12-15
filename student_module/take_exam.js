683f96563f7640f2b58fef0c0133e2835b1c0f01
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-15 18:53:30 +0800

    v103

--- student_module/take_exam.js
+++ student_module/take_exam.js
@@ -121,7 +121,7 @@ function loadExam()
 
                 //display the first question
                 displayQuestion(currentQNum);
-                displayButtons(currentQNum);
+                displayButtons(currentQNum);//try
             }
             break;
         }
