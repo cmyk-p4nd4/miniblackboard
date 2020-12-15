fd2ab50aa57efa5be6140aa8e390daf23a9abb4e
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-15 17:18:37 +0800

    add checkStatistics

---
+++ instructor_module/exam/checkStatistics.js
@@ -0,0 +1,10 @@
+
+
+
+var overallDiv = document.getElementById("overallDiv");
+var questionDiv = document.getElementById("questionDiv");
+var buttonDiv = document.getElementById("buttonDiv");
+function init()
+{  
+    
+}
--- instructor_module/exam/checkStatistics.php
+++ instructor_module/exam/checkStatistics.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="registerExam.css"/>
 	<script src="checkStatistics.js"></script>
 </head>
-<body>
+<body onload="init();">
     <?php
         session_start();
         $examID = $_POST['refExamID'];
@@ -18,7 +18,29 @@
         ."</p>"
         ."</div>";
 
-        //do a set of query to retrieve 
+        
     ?>
+    <h1 align='center' id='titleH1'></h1>
+    <div id="overallDiv">
+        <p id='refExamID' hidden><?php echo $_POST['refExamId']?></p>;
+        <p id='refExamPrefix' hidden><?php echo $_POST['refExamPrefix']?></p>
+        <p>Overall statistics: </p>
+        <p id='studentCountLbl'></p>
+        <p id='omaxLbl'></p>
+        <p id='ominLbl'></p>
+        <p id='omedianLbl'></p>
+        <p id='oaverageLbl'></p>
+    </div>
+
+    <div id="questionDiv">
+        <h2 id='questionDivH2'>Statistics of each question</h2>
+        <p id='questionSelect'>Question: <select id='qSelect'></select></p>
+        <p id='questionDisp'></p>
+        <p id='percentageDisp'></p>
+        <p id='averageDisp'></p>
+    </div>
+
+    <div id="buttonDiv">
+    </div>
 </body>
 </html>
