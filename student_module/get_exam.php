bd0a94a3b23575b17433bc7f13aa8595a50212ef
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-15 17:58:57 +0800

    bugfix

--- student_module/get_exam.php
+++ student_module/get_exam.php
@@ -8,30 +8,34 @@
     $db = "heroku_2d30a0192d19383";
     $connect = mysqli_connect($server,$user,$pw,$db);
 
-    if !($connect)
+    if (!$connect)
     {
         print "FailConnection";
-        die();
+        //die();
+    }
+    else 
+    {
+        $query = "SELECT questions FROM exams WHERE exam_name='".$exam_name."';";
+        $result = mysqli_query($connect,$query);
+
+        if (!$result)
+        {
+            print "FailQuery";
+            //die();
+        }
+
+        //suppose only one row
+        if (mysqli_num_rows($result) > 0)
+        {
+            $row = mysqli_fetch_assoc($result);
+            print $row['questions'];
+        } else
+        {
+            print "NoResultFound";
+        }
     }
 
-    $query = "SELECT questions FROM exams WHERE exam_name='".$exam_name."';";
-    $result = mysqli_query($connect,$query);
-
-    if (!$result)
-    {
-        print "FailQuery";
-        die();
-    }
-
-    //suppose only one row
-    if (mysqli_num_rows($result) > 0)
-    {
-        $row = mysqli_fetch_assoc($result);
-        print $row['questions'];
-    } else
-    {
-        print "NoResultFound";
-    }
+    
     
 
 
