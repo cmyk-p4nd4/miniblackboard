585218236a810fd7f48063c00bbd4745d0bd9713
sfcgta4794 <63903320+sfcgta4794@users.noreply.github.com>
2020-12-15 14:03:11 +0800

    finished take exam functions and submit ajax command (15/12/2020 14:03)

---
+++ student_module/submit_exam.php
@@ -0,0 +1,51 @@
+<?php
+    $server = "us-cdbr-east-02.cleardb.com";
+	$user = "b06abb5474de88";
+	$pw = "5b46e8a1";
+    $db = "heroku_2d30a0192d19383";
+    $connect = mysqli_connect($server,$user,$pw,$db);
+
+    if !($connect)
+    {
+        die("FailConnection");
+    }
+
+    $exam_name = $_POST['exam_name'];
+    $userid = $_POST['userid'];
+    $submitTime = $_POST['submit_time'];
+    $questionAnswers = $_POST['questionanswers'];
+    $student_answer = $_POST['student_answer'];
+    $marking = $_POST['marking'];
+    $totalmarks = $_POST['totalmarks'];
+
+    //retrieve exam id 
+    $query = "SELECT DISTINCT examid FROM exams where exam_name='".$exam_name."'";
+    $result = mysqli_query($connect,$result);
+
+    if (!$result)
+    {
+        die("FailQuery");
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    $exam_id = $row['examid'];
+
+    $query = "INSERT INTO exam_records (`examid`,`studentid`,`submit_time`,`questionanswers`,`student_answer`,`marking`,`totalmarks`) VALUES ".
+    "('".$exam_id."','".$userid."','".$submitTime."','".$questionAnswers."','".$student_answer."','".$marking."','".$totalmarks."');";
+    $result = mysqli_query($connect,$result);
+
+    if (!$result)
+    {
+        die("FailQuery");
+    }
+
+    if (mysqli_affected_rows($connect) > 0)
+    {
+        print "Added";
+    }
+    else
+    {
+        print "NoEffects";
+    }
+
+?>
--- student_module/take_exam.js
+++ student_module/take_exam.js
@@ -3,8 +3,10 @@ var startTime = "";
 var deadline = "";
 var userid = "";
 var userName = "";
+var studentAns = [];
 
 var originalQuestionTxt = "";
+var backupQuestionTxt = "";
 var questions = [];
 var questionType = [];
 var answers = [];
@@ -16,6 +18,8 @@ function loadExam()
 {
     //retrieve the cookies first
     //exam_name, start_time, deadline, userid 
+    var debugging = false;
+
     var existingCookies = document.cookie;
     existingCookies = decodeURIComponent(existingCookies);
     existingCookies = existingCookies.split(";");
@@ -83,6 +87,7 @@ function loadExam()
     requestQuestion.send("userid="+userid+"&exam_name="+exam_name);
 
     //suppose all failure response has been redirected.
+    backupQuestionTxt = originalQuestionTxt;
     originalQuestionTxt = JSON.parse(originalQuestionTxt);
     questions = JSON.parse(originalQuestionTxt[0]);
     questionType = JSON.parse(originalQuestionTxt[1]);
@@ -95,12 +100,286 @@ function loadExam()
     questionCntDisp.innerHTML = "There are a total of "+questions.length+" questions.";
     document.getElementById("basicInfo").appendChild(questionCntDisp);
 
-    //display the first questions
-
+    //display the first question
+    displayQuestion(currentQNum);
 
 }
 
 function displayQuestion(qNum)
 {
-    
+    //clear the questionDiv first
+    document.getElementById("questionDiv").innerHTML = "";
+    //detect questionType
+    switch (questionType[qNum])
+    {
+        case "MCQ":
+        {
+            var questionNoDisp = document.createElement("p");
+            questionNoDisp.setAttribute("id","questionNoTxt");
+            questionNoDisp.innerHTML = "Question "+(qNum + 1);
+            document.getElementById("questionDiv").appendChild(questionNoDisp);
+
+            var questionDisp = document.createElement("p");
+            questionDisp.setAttribute("id","questionDisp");
+            questionDisp.innerHTML = "Question: " + questions[qNum];
+            document.getElementById("questionDiv").appendChild(questionDisp);
+
+            var thisAnswers = JSON.parse(answers[qNum]);
+
+            for (var i = 0; i < thisAnswers.length; i++)
+            {
+                var answerRadio = document.createElement("input");
+                answerRadio.setAttribute("type","radio");
+                answerRadio.setAttribute("id","answer"+i);
+                answerRadio.setAttribute("name","answer");
+                answerRadio.setAttribute("value",thisAnswers[i]);
+            }
+
+
+        }
+        break;
+        case "SQ":
+        case "FB":
+        {
+            var questionNoDisp = document.createElement("p");
+            questionNoDisp.setAttribute("id","questionNoTxt");
+            questionNoDisp.innerHTML = "Question "+(qNum + 1);
+            document.getElementById("questionDiv").appendChild(questionNoDisp);
+
+            var questionDisp = document.createElement("p");
+            questionDisp.setAttribute("id","questionDisp");
+            questionDisp.innerHTML = "Question: " + questions[qNum];
+            document.getElementById("questionDiv").appendChild(questionDisp);
+
+            var answerDisp = document.createElement("p");
+            answerDisp.setAttribute("id","answerDisp");
+            answerDisp.innerHTML = "Answer: ";
+            document.getElementById("answerDisp").appendChild(answerDisp);
+
+            var answerInput = document.createElement("input");
+            answerInput.setAttribute("type","text");
+            answerInput.setAttribute("id","answer");
+            answerInput.setAttribute("name","answer");
+            answerDisp.appendChild(answerInput);
+        }
+        break;
+    }
+}
+
+function saveQuestion(qNum)
+{
+    switch (questionType[qNum])
+    {
+        case "MCQ":
+        {
+            var radioGroup = document.getElementsByName("answer");
+            for (var i = 0; i < radioGroup.length; i++)
+            {
+                if (radioGroup[i].checked)
+                {
+                    //record into studentAns
+                    studentAns[qNum] = radioGroup[i].value;
+                    break;
+                }
+            }
+        }
+        break;
+        case "SQ":
+        case "FB":
+        {
+            var answerBox = document.getElementById("answer");
+            studentAns[qNum] = answerBox.value;
+        }
+        break;
+    }
+}
+
+function displayButtons(qNum)
+{
+    document.getElementBtId("buttonList").innerHTML="";
+    var prevBtn = document.createElement("button");
+    prevBtn.setAttribute("id","prevBtn");
+    prevBtn.setAttribute("onclick","prevQuestion();");
+    prevBtn.innerHTML = "Previous Question";
+    if (qNum <= 0)
+        prevBtn.setAttribute("disabled","true");
+    else
+        prevBtn.setAttribute("disabled","false");
+    document.getElementById("buttonList").appendChild(prevBtn);
+
+    var nextBtn = document.createElement("button");
+    nextBtn.setAttribute("id","nextBtn");
+    nextBtn.setAttribute("onclick","nextQuestion();");
+    nextBtn.innerHTML = "Next Question";
+    if (qNum >= questions.length - 1)
+        nextBtn.setAttribute("disabled","true");
+    else
+        nextBtn.setAttribute("disabled","false");
+    document.getElementById("buttonList").appendChild(nextBtn);
+
+    var submitBtn = document.createElement("button");
+    submitBtn.setAttribute("id","submitBtn");
+    submitBtn.setAttribute("onclick","submitWork();");
+    submitBtn.innerHTML = "Submit exam";
+    document.getElementById("buttonList").appendChild(submitBtn);
+}
+
+function prevQuestion()
+{
+    saveQuestion(currentQNum);
+    currentQNum--;
+    displayQuestion(currentQNum);
+    displayButtons(currentQNum);
+}
+
+function nextQuestion()
+{
+    saveQuestion(currentQNum);
+    currentQNum++;
+    displayQuestion(currentQNum);
+    displayButtons(currentQNum);
+}
+
+function submitWork()
+{
+    //check if student has answered all questions
+    //to deal with clicking the submit button after finishing the last question,
+    //saveQuestion for once first
+    saveQuestion(currentQNum);
+
+    var properlyFilled = true;
+
+    //check if the student has completed all answer
+    if (studentAns.length < questions.length)
+    {
+        properlyFilled = false;
+    } else
+    {
+        //check if the answer has any empty. if empty found, then also !properlyFilled
+        for (var i = 0; i < studentAns.length; i++)
+        {
+            if (studentAns[i] == 'undefined')
+                properlyFilled = false;
+            else if (studentAns[i] == "")
+                properFilled = false;
+            if (!properFilled)
+                break;
+        }
+    }
+
+    var toSubmit = false;
+    if (!properlyFilled)
+    {
+        if (confirm("You still have some questions not finished, confirm to submit?"))
+            toSubmit = true;
+    }
+    else
+    {
+        toSubmit = true;
+    }
+
+    if (toSubmit)
+    {
+        //find if there is any empty answer
+        for (var i = 0; i < questions.length; i++)
+        {
+            if (studentAns[i] == 'undefined')
+            {
+                studentAns[i] == "";
+            }
+        }
+
+        //check MC question answer, and put "?" to FB, SQs
+        var marking = [];
+        var totalMarks = 0;
+        for (var j = 0; j < questions.length; j++)
+        {
+            //one question
+            //fetch the mark of this question
+            var thisQuestionScore = marksAvailable[j]
+            switch (questionType[j])
+            {
+                case "MCQ":    
+                {
+                    //get which answer is correct
+                    var correctAnsArray = JSON.parse(correctAnswers[j]);
+                    var AnsArray = JSON.parse(answers[j]);
+                    var correctAnsPos = 0;
+                    for (var t = 0; t < correctAnsArray.length; t++)
+                    {
+                        if (correctAnsArray[t] == 1)
+                            correctAnsPos = t;
+                    }
+                    if (studentAns[j] == AnsArray[t])
+                    {
+                        totalMarks += thisQuestionScore;
+                        marking[j] = thisQuestionScore;
+                    }
+                }
+                break;
+                case "SQ":
+                case "FB":
+                {
+                    //SQ and FB no need to grade
+                    marking[j] = "?";
+                    totalMarks += 0;
+                }
+                break;
+            }
+        }
+
+        //encode student's answers and marking
+        var JSONstdAnswer = JSON.stringify(studentAns);
+        var JSONmarking = JSON.stringify(marking);
+
+        //generate the submittion date
+        var submitDate = new Date();
+        var submitDateStr = submitDate.getFullYear()+"-"+(submitDate.getMonth()+1)+"-"+(submitDate.getDate()+1)
+        +" "+(submitDate.getHours())+":"+(submitDate.getMinutes())+":"+(submitDate.getSeconds());
+        
+
+        //send stdAnswer, studentid, time of submit, backup...(original text), marking and totalmarks
+        //do an SQL query to get the questions
+        var requestQuestion = new XMLHttpRequest();
+        requestQuestion.open("POST","submit_exam.php");
+        requestQuestion.setRequestHeader('content-type','application/x-www-form-urlencoded; charset=UTF-8');
+        requestQuestion.onload=function()
+        {
+            var response = requestQuestion.responseText;
+            switch (response)
+            {
+                case "FailConnection":
+                {
+                    alert("Fail to connect with server!");
+                    //window.href.location="https://web-miniblackboard.herokuapp.com/wrap/std-courseDisplay.php";
+                }
+                break;
+                case "FailQuery":
+                {
+                    alert("Fail to insert your record!");
+                    //window.href.location="https://web-miniblackboard.herokuapp.com/wrap/std-courseDisplay.php";
+                }
+                break;
+                default:
+                {
+                    var posResponse = response;
+                    if (posResponse == "Added")
+                    {
+                        alert("Submit successful!");
+                        window.href.location="https://web-miniblackboard.herokuapp.com/wrap/std-courseDisplay.php";
+                    } else if (posResponse == "NoEffects")
+                    {
+                        alert("Seems you have attempted this test before!");
+                        window.href.location="https://web-miniblackboard.herokuapp.com/wrap/std-courseDisplay.php";
+                    }
+                }
+                break;
+        }
+        };
+        //
+        requestQuestion.send("exam_name="+exam_name+"&userid="+userid+"&submit_time="+submitDateStr+
+        "&questionanswers="+backupQuestionTxt+"&student_answer="+JSONstdAnswer+"&marking="+JSONmarking+
+        "&totalmarks="+totalMarks);
+    }
+
 }
